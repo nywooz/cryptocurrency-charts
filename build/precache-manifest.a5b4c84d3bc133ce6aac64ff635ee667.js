9f89ea7c9bc237c7da12231f3cc72b54252b57e3
nywooz <nywooz7@gmail.com>
2019-10-17 00:54:04 +0100

    started build

---
+++ build/precache-manifest.a5b4c84d3bc133ce6aac64ff635ee667.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056059d07cf6daecd05cf182da6125f3",
+    "url": "/cryptocurrency-charts/index.html"
+  },
+  {
+    "revision": "b034385d55e02f7b67ed",
+    "url": "/cryptocurrency-charts/static/js/2.359df21f.chunk.js"
+  },
+  {
+    "revision": "256ab8e2612f574afde3",
+    "url": "/cryptocurrency-charts/static/js/main.a60a32b5.chunk.js"
+  },
+  {
+    "revision": "be9e85b0dfd9fabd7a6c",
+    "url": "/cryptocurrency-charts/static/js/runtime-main.5a044fb7.js"
+  }
+]);
